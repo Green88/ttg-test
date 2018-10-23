e15f5b7fab990c5912eaf75ed2de6656812c7aa7
Green88 <taniagr0808@gmail.com>
2018-10-23 16:52:07 +0300

    remove log

--- src/utils/request-validator.js
+++ src/utils/request-validator.js
@@ -3,7 +3,6 @@ const Joi = require('joi');
 module.exports = (obj, schema) => {
     const { error, value } = Joi.validate(obj, schema);
     if (error) {
-        console.log(error);
         return false;
     }
     return value;
